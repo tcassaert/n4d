82c414097d302f7952c9b1f3668a4771975ab6a6
tcassaert <tcassaert@inuits.eu>
2021-01-29 17:31:14 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/tcassaert/n4d
+
+go 1.15
+
+require (
+	github.com/hashicorp/go-hclog v0.15.0
+	github.com/hashicorp/nomad/api v0.0.0-20210115191909-bcd4752fc902
+)
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/nomad/api"
+)
+
+var (
+	help = flag.Bool("help", false, "Print help message")
+	url  = flag.String("url", "", "The Nomad API address")
+	job  = flag.String("job", "*", "The job to catch the eventstream for (defaults to all jobs)")
+)
+
+type Stream struct {
+	nomad *api.Client
+	L     hclog.Logger
+}
+
+func main() {
+	flag.Parse()
+
+	if *help == true {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	ctx, closer := CtxWithInterrupt(context.Background())
+	defer closer()
+
+	stream := NewStream()
+
+	events := stream.nomad.EventStream()
+
+	topics := map[api.Topic][]string{
+		api.Topic("Allocation"): {*job},
+	}
+	eventCh, err := events.Stream(ctx, topics, 0, &api.QueryOptions{})
+	if err != nil {
+		stream.L.Error("Error creating event stream client", "error", err)
+		os.Exit(1)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case event := <-eventCh:
+			if event.Err != nil {
+				stream.L.Warn("Error from event stream", "error", err)
+				break
+			}
+
+			if event.IsHeartbeat() {
+				continue
+			}
+
+			for _, e := range event.Events {
+				alloc, err := e.Allocation()
+				if err != nil {
+					stream.L.Error("Expected allocation", "error", err)
+					continue
+				}
+
+				alloc_json, err := json.Marshal(alloc)
+				var allocations api.Allocation
+				err = json.Unmarshal(alloc_json, &allocations)
+
+				if err != nil {
+					stream.L.Error("Error parsing JSON", "error", err)
+				}
+
+				taskstate_json, err := json.Marshal(allocations.TaskStates)
+				var taskstates api.TaskState
+				err = json.Unmarshal(taskstate_json, &taskstates)
+
+				if err != nil {
+					stream.L.Error("Error parsing JSON", "error", err)
+				}
+
+				events_json, err := json.Marshal(taskstates.Events)
+				var events api.Events
+				err = json.Unmarshal(events_json, &events)
+
+				if err != nil {
+					stream.L.Error("Error parsing JSON", "error", err)
+				}
+
+				if allocations.TaskStates[allocations.JobID] != nil {
+					events_length := len(allocations.TaskStates[allocations.JobID].Events)
+					timestamp := time.Unix(0, allocations.TaskStates[allocations.JobID].Events[events_length-1].Time)
+					fmt.Printf("timestamp=%s, node=%s, alloc_id=%s, job_id=%s, namespace=%s, job_status=%s, task_state=%s, event_type=%s, event=%s \n",
+						timestamp,
+						allocations.NodeName,
+						allocations.ID,
+						allocations.JobID,
+						allocations.Namespace,
+						allocations.ClientStatus,
+						allocations.TaskStates[allocations.JobID].State,
+						allocations.TaskStates[allocations.JobID].Events[events_length-1].Type,
+						allocations.TaskStates[allocations.JobID].Events[events_length-1].DisplayMessage,
+					)
+				}
+			}
+		}
+	}
+}
+
+func NewStream() *Stream {
+	client, _ := api.NewClient(&api.Config{
+		Address: *url,
+		TLSConfig: &api.TLSConfig{
+			Insecure: true,
+		},
+	})
+
+	return &Stream{
+		nomad: client,
+		L:     hclog.Default(),
+	}
+}
+
+func CtxWithInterrupt(ctx context.Context) (context.Context, func()) {
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-ch:
+			cancel()
+		case <-ctx.Done():
+			return
+		}
+	}()
+
+	return ctx, func() {
+		signal.Stop(ch)
+		cancel()
+	}
+}
